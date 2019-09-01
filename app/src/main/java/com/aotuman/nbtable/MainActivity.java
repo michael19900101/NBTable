7dcf5bcb408acc872f1f854fddfe155e6c7eca1a
Michael <382968311@qq.com>
2019-09-01 21:05:09 +0800

    实现竖向滚动

--- app/src/main/java/com/aotuman/nbtable/MainActivity.java
+++ app/src/main/java/com/aotuman/nbtable/MainActivity.java
@@ -1,14 +1,29 @@
 package com.aotuman.nbtable;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.aotuman.nbtable.util.DensityUtil;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private XWLayoutManager xwLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        xwLayoutManager = new XWLayoutManager(this, DensityUtil.dip2px(this, 700));
+        recyclerView.setLayoutManager(xwLayoutManager);
+        final XWRecyclerViewAdapter adapter = new XWRecyclerViewAdapter(recyclerView);
+        recyclerView.setAdapter(adapter);
+        final List<HorizontalBean> beans = HorizontalBean.initDatas();
+        adapter.setData(beans);
     }
 }
